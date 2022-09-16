f32360ea865e5f46da0c5b981b93081b996691a7
wiryadev <abrarwiryawan@gmail.com>
2022-09-16 20:03:43 +0700

    use SettingPreferences in MainActivity

--- app/src/main/java/com/wiryadev/preferencesample/MainActivity.kt
+++ app/src/main/java/com/wiryadev/preferencesample/MainActivity.kt
@@ -2,15 +2,39 @@ package com.wiryadev.preferencesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.wiryadev.preferencesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var settingPrefs: SettingPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        settingPrefs = SettingPreferences(this)
+
+        checkDarkModeStatus()
+
+        binding.swTheme.setOnCheckedChangeListener { _, isChecked ->
+            settingPrefs.saveThemeSetting(isChecked)
+            checkDarkModeStatus()
+        }
+    }
+
+    private fun checkDarkModeStatus() {
+        val isDarkModeActive = settingPrefs.getThemeSetting()
+        if (isDarkModeActive) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            binding.swTheme.isChecked = true
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            binding.swTheme.isChecked = false
+        }
     }
 
 }
