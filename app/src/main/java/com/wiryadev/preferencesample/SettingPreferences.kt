6825ed896f99de70d42a01c2851837decdcf3212
wiryadev <abrarwiryawan@gmail.com>
2022-09-16 20:02:53 +0700

    create SettingPreferences

---
+++ app/src/main/java/com/wiryadev/preferencesample/SettingPreferences.kt
@@ -0,0 +1,23 @@
+package com.wiryadev.preferencesample
+
+import android.content.Context
+
+class SettingPreferences(context: Context) {
+
+    private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun saveThemeSetting(isDarkModeActive: Boolean) {
+        val editor = preferences.edit()
+        editor.putBoolean(THEME_SETTING, isDarkModeActive)
+        editor.apply()
+    }
+
+    fun getThemeSetting(): Boolean {
+        return preferences.getBoolean(THEME_SETTING, false)
+    }
+
+    companion object {
+        private const val PREFS_NAME = "settings"
+        private const val THEME_SETTING = "theme_setting"
+    }
+}
