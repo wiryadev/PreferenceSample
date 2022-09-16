cbee083e29b214d3d52fc0668c47d8d393cb8c40
wiryadev <abrarwiryawan@gmail.com>
2022-09-16 19:49:07 +0700

    enable ViewBinding and add main layout

--- app/src/main/java/com/wiryadev/preferencesample/MainActivity.kt
+++ app/src/main/java/com/wiryadev/preferencesample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.wiryadev.preferencesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wiryadev.preferencesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
